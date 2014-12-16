8d5259637d21e6d791b397d5e6e0492d9dc1fccc
Brian Hou <brian.hou@live.com>
2014-12-16 03:56:39 -0800

    basic demo

--- final/samples.py
+++ final/samples.py
@@ -1,16 +1,18 @@
-from dragonsmash import *
-from dragonsmash.util import *
+from client import *
 
-def tdfw(c):
-    c.addLoopedNote(Note(note_to_num['E6'], 13, 8, 127), 0, 64)
-    c.addLoopedNote(Note(note_to_num['E6'], 13, 4, 127), 8, 64)
-    c.addLoopedNote(Note(note_to_num['C6'], 13, 8, 127), 12, 64)
-    c.addLoopedNote(Note(note_to_num['B5'], 13, 12, 127), 24, 64)
-    c.addLoopedNote(Note(note_to_num['B5'], 13, 4, 127), 40, 64)
-    c.addLoopedNote(Note(note_to_num['C6'], 13, 8, 127), 44, 64)
-    c.addLoopedNote(Note(note_to_num['E5'], 13, 8, 127), 56, 64)
+def tdfw(volume=127):
+    notes = [
+        Note('E6', 13, 0.5,  volume, 0),
+        Note('E6', 13, 0.25, volume, 0.5),
+        Note('C6', 13, 0.5,  volume, 0.75),
+        Note('B5', 13, 0.75, volume, 1.5),
+        Note('B5', 13, 0.25, volume, 2.5),
+        Note('C6', 13, 0.5,  volume, 2.75),
+        Note('E5', 13, 0.5,  volume, 3.5),
+    ]
 
-s = Speaker(16, 64)
-tdfw(s.newConnection())
-s.playAll()
+    for note in notes:
+        player.loop(note, 4)
 
+if __name__ == '__main__':
+    tdfw(volume=32)
